d6bbe4118c598b591d7b37bb8f3554293147aa3c
r0r0Sec <r0r0sec@proton.me>
2025-01-04 16:36:26 +0100

    Delete SoundChanger.cmd

--- SoundChanger.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-SET ThisScriptsDirectory=%~dp0
-SET PowerShellScriptPath=%ThisScriptsDirectory%AudioDevicechanger.ps1
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' ";
